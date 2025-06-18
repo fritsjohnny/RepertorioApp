5a0e1ee744ae3bbad92a9a090bd901c294f40861
Johnny Frits <johnny.frits@outlook.com>
2025-06-17 22:45:21 -0400

    Criado o projeto

---
+++ RepertorioApp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LouvorListComponent } from './pages/louvor-list/louvor-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LouvorListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  protected title = 'RepertorioApp';
+}
---
+++ RepertorioApp/src/main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(FormsModule), provideAnimations()],
+});
